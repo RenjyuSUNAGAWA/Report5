820003bde2b002685c88537db6759fd6e1e15c51
Renjyu SUNAGAWA <e235747@ie.u-ryukyu.ac.jp>
2023-12-11 10:50:36 +0900

    2回目

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+//import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args){ //throws Exception {
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("NullpoiterException occurred.");
+            System.out.println("Exception message: " + e.getMessage());
+        }
     }
 }
